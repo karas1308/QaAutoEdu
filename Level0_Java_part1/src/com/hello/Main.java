1215100243aa05f980aff9a8cf78daa44adf7e36
ihor.shchuka <ihor.shchuka@yandex.ru>
2017-01-05 22:42:53 +0200

    hello

--- Level0_Java_part1/src/com/hello/Main.java
+++ Level0_Java_part1/src/com/hello/Main.java
@@ -1,24 +1,20 @@
 package com.hello;
+
 import com.welcome.Hello;
+
 import java.util.Scanner;
 
 public class Main {
     private static String name;
 
-    public String getName(){
-        return name;
-    }
     public static void main(String[] args) {
-
         System.out.println("Как тебя зовут?");
         Scanner in = new Scanner(System.in); //Ведите имя
-        String name = in.nextLine();
+        name = in.nextLine();
         Hello test = new Hello();
         test.setupName(name);
         test.welcome();                                 // System.out.println("Hello, " +name1);
         System.out.println("Hello, world!!!");
         test.byeBye();                                  //System.out.println("byeBye, " + name1);
     }
-
-
 }
--- Level0_Java_part1/src/com/welcome/Hello.java
+++ Level0_Java_part1/src/com/welcome/Hello.java
@@ -1,19 +1,15 @@
 package com.welcome;
 
 public class Hello {
-   private String name;
-        public void setupName(String name) {
-            this.name=name;
-        }
+    private String name;
 
+    public void setupName(String name) {
+        this.name = name;
+    }
     public void welcome() {
         System.out.println("Hello, " + name);
-
     }
-
     public void byeBye() {
         System.out.println("byeBye, " + name);
     }
-
-
 }
