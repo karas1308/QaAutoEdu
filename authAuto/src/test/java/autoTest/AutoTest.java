876f9f336d3693b0c683442bfb00c56aaf4132d2
ihor.shchuka <ihor.shchuka@yandex.ru>
2017-02-16 01:37:57 +0200

    Some DDT + Some new tests Api AUTO

--- authAuto/src/test/java/autoTest/AutoTest.java
+++ authAuto/src/test/java/autoTest/AutoTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -31,6 +32,7 @@ import org.junit.runners.Parameterized;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,6 +45,7 @@ public class AutoTest {
     public static String autoruuid;
     public static String x_auth;
     public static long millis = System.currentTimeMillis();
+    public static String url_api2 = "https://api2.auto.ru/1.1/search?category_id=15&page_num=1&page_size=50&creation_date_to=" + millis;
     String username = "yuioru@yandex.ru";
     String password = "111111";
 
@@ -53,28 +56,20 @@ public class AutoTest {
     public static void beforeClass() throws IOException {
         json = FirstConnectJson.json();
         sid = String.valueOf(json.get("sid"));
-        uuid ="OAuth" +  " " + String.valueOf(json.getJSONObject("result").get("uuid"));
-        x_auth = "Vertis" + " " + String.valueOf(json.getJSONObject("result").get("uuid"))+ " " +"5c27f9e8-2b90-433e-a0bf-b5222bbd97d0";
+        uuid = "OAuth" + " " + String.valueOf(json.getJSONObject("result").get("uuid"));
+        x_auth = "Vertis" + " " + String.valueOf(json.getJSONObject("result").get("uuid")) + " " + "5c27f9e8-2b90-433e-a0bf-b5222bbd97d0";
         autoruuid = String.valueOf(json.get("autoruuid"));
     }
 
     @Test
     public void NumberRegions() throws IOException {
 
-//        System.out.println(json);
-//        System.out.println(sid);
-//        System.out.println(uuid);
         HttpGet get = new HttpGet("https://api2.auto.ru/1.1/regions");
         get.setHeader("Authorization", uuid);
         get.setHeader("X-Authorization", x_auth);
         CloseableHttpResponse response = client.execute(get);
         HttpEntity entity = response.getEntity();
         JSONArray regions = new JSONArray(EntityUtils.toString(entity, UTF_8));
-        // regions.optJSONObject(0);
-        System.out.println(regions.getJSONObject(1));
-        System.out.println(regions.getJSONObject(1).get("rid"));
-        String cp866 = new String(regions.getJSONObject(1).get("name").toString().getBytes(), "windows-1251");
-        System.out.print(cp866);
     }
 
     @Test
@@ -84,40 +79,88 @@ public class AutoTest {
 //        System.out.println(sid);
 //        System.out.println(uuid);
         HttpGet get = new HttpGet("https://api2.auto.ru/1.1/regions/10841");
-        get.setHeader("Authorization", "OAuth eb33980619853b4fab16f0349ca64c78");
-        get.setHeader("X-Authorization", "Vertis eb33980619853b4fab16f0349ca64c78 5c27f9e8-2b90-433e-a0bf-b5222bbd97d0");
+        get.setHeader("Authorization", uuid);
+        get.setHeader("X-Authorization", x_auth);
         get.setHeader("Accept-Encoding", "gzip, deflate, identity");
         get.setHeader("Accept-Language", "en-RU;q=1, ru-RU;q=0.9, uk-RU;q=0.8, it-RU;q=0.7, de-RU;q=0.6");
         CloseableHttpResponse response = client.execute(get);
         HttpEntity entity = response.getEntity();
         JSONArray regions = new JSONArray(EntityUtils.toString(entity, UTF_8));
         System.out.println(regions.get(1));
-        System.out.println();
-        String cp866 = new String(String.valueOf(regions.getJSONObject(1).get("name")));
-        System.out.print(cp866);
+
     }
 
     @Test
     public void test() throws IOException {
 
-        System.out.println(json);
-        System.out.println(sid);
-        System.out.println(uuid);
         HttpGet get = new HttpGet("https://api.auto.ru/rest/?sid=&client_tz=180&method=api.service.getUuid&client_version=3.10.1&key=1d2b14555a83699f57fd77d17aa2d5ce9431cd7d9f3edea14186b044e76b606a&client_os=6.0.1&version=2.2.2&device_name=motorola%20XT1562&client_platform=android&format=json");
         get.setHeader("Authorization", uuid);
-        get.setHeader("X-Authorization", "Vertis eb33980619853b4fab16f0349ca64c78 5c27f9e8-2b90-433e-a0bf-b5222bbd97d0");
+        get.setHeader("X-Authorization", x_auth);
         CloseableHttpResponse response = client.execute(get);
         HttpEntity entity = response.getEntity();
-        JSONObject test = new JSONObject(EntityUtils.toString(entity, UTF_8));
+        JSONObject jsonTets = new JSONObject(EntityUtils.toString(entity, UTF_8));
         System.out.println(response);
+    }
+
+    @Test //Тест проверяет корректность выдачи по фильтру Цена
+    public void testPrice() throws IOException {
+        int minprice = 500000;
+        int maxprice = 600000;
+        HttpGet get = new HttpGet(url_api2+"&price_from="+minprice+"&price_to="+maxprice);
+        get.setHeader("Authorization", uuid);
+        get.setHeader("X-Authorization", x_auth);
+        CloseableHttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        JSONObject jsonTets = new JSONObject(EntityUtils.toString(entity, UTF_8));
+        int lenght = jsonTets.getJSONArray("list").length();
+        for (int i = 0; i < lenght; i++) {
+            String[] priceList = (String.valueOf(jsonTets.getJSONArray("list").getJSONObject(i).get("price")).replace("}", "").split(":"));
+            int price = Integer.valueOf(priceList[3]);
+            assertTrue("Цена выходит за параметры фильтров - " + price, price>=minprice & price<=maxprice);
+            //System.out.println(jsonTets.getJSONArray("list").getJSONObject(i).get("price"));
+        }
+
+    }
+
+    @Test //Тест проверяет корректность выдачи по фильтру Год выпуска
+    public void yearProd() throws IOException {
+        int year_from = 1995;
+        int year_to = 1996;
+        HttpGet get = new HttpGet(url_api2+"&year_from="+year_from+"&year_to="+year_to);
+        get.setHeader("Authorization", uuid);
+        get.setHeader("X-Authorization", x_auth);
+        CloseableHttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        JSONObject jsonTets = new JSONObject(EntityUtils.toString(entity, UTF_8));
+       // System.out.println(jsonTets.getJSONArray("list").getJSONObject(0).get("year"));
+        int lenght = jsonTets.getJSONArray("list").length();
+        for (int i = 0; i < lenght; i++) {
+            String yearStr = String.valueOf(jsonTets.getJSONArray("list").getJSONObject(i).get("year"));
+            int year = Integer.valueOf(yearStr);
+            assertTrue("Год выпуска выходит за параметры фильтров - " + year, year>=year_from & year<=year_to);
+        }
+    }
+    @Test //Тест проверяет корректность выдачи по фильтру Пробег
+    public void km_age() throws IOException {
+        int km_age_from = 40000;
+        int km_age_to = 42000;
+        HttpGet get = new HttpGet(url_api2+"&km_age_from="+km_age_from+"&km_age_to="+km_age_to);
+        get.setHeader("Authorization", uuid);
+        get.setHeader("X-Authorization", x_auth);
+        CloseableHttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        JSONObject jsonTets = new JSONObject(EntityUtils.toString(entity, UTF_8));
+        int lenght = jsonTets.getJSONArray("list").length();
+        for (int i = 0; i < lenght; i++) {
+            String km_ageStr = String.valueOf(jsonTets.getJSONArray("list").getJSONObject(i).get("km_age"));
+            int km_age = Integer.valueOf(km_ageStr);
+            assertTrue("Пробег выходит за параметры фильтров - " + km_age, km_age>=km_age_from & km_age<=km_age_to);
+        }
     }
 
     @Test
     public void mark_list() throws IOException {
 
-        System.out.println(json);
-        System.out.println(sid);
-        System.out.println(uuid);
         HttpGet get = new HttpGet("https://api.auto.ru/rest/?method=all.mark.getList&category_id=15&sid=e21f5804f95d3252_87ac09264e00d4026dbbfbdee316b702&client_tz=180&method=all.mark.getList&client_version=3.10.1&key=1d2b14555a83699f57fd77d17aa2d5ce9431cd7d9f3edea14186b044e76b606a&client_os=6.0.1&version=2.2.2&uuid=1a63c0971a6d72f43156c4fe2a8edf98&device_name=motorola%20XT1562&client_platform=android&format=json");
         get.setHeader("Authorization", uuid);
         get.setHeader("X-Authorization", x_auth);
@@ -126,7 +169,6 @@ public class AutoTest {
         JSONObject test = new JSONObject(EntityUtils.toString(entity, UTF_8));
         System.out.println(test);
     }
-
 
     @Test
     public void testAutori() throws IOException {
@@ -148,14 +190,14 @@ public class AutoTest {
         HttpPost httpost = new HttpPost(url);
 
         StringEntity entity = new StringEntity(json.toString());
-          //entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+        //entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
         httpost.setEntity(entity);
 
         //   HttpResponse response = client.execute(httpost);
 
         CloseableHttpResponse response = client.execute(httpost);
         HttpEntity entity1 = response.getEntity();
-       // JSONObject test = new JSONObject(EntityUtils.toString(entity1));
+        // JSONObject test = new JSONObject(EntityUtils.toString(entity1));
 
 
         //HttpEntity entity = response.getEntity();
--- authAuto/src/test/java/autoTest/TestDDT.java
+++ authAuto/src/test/java/autoTest/TestDDT.java
@@ -20,6 +20,7 @@ import java.util.Collection;
 import java.util.Date;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(value = Parameterized.class)
 
@@ -30,6 +31,7 @@ public class TestDDT {
     public static String autoruuid;
     public static String x_auth;
     public static long millis = System.currentTimeMillis();
+    public static String url_api2 = "https://api2.auto.ru/1.1/search?category_id=15&page_num=1&page_size=50&creation_date_to=" + millis;
     String username = "yuioru@yandex.ru";
     String password = "111111";
 
@@ -40,24 +42,46 @@ public class TestDDT {
     public static void beforeClass() throws IOException {
         json = FirstConnectJson.json();
         sid = String.valueOf(json.get("sid"));
-        uuid ="OAuth" +  " " + String.valueOf(json.getJSONObject("result").get("uuid"));
-        x_auth = "Vertis" + " " + String.valueOf(json.getJSONObject("result").get("uuid"))+ " " +"5c27f9e8-2b90-433e-a0bf-b5222bbd97d0";
+        uuid = "OAuth" + " " + String.valueOf(json.getJSONObject("result").get("uuid"));
+        x_auth = "Vertis" + " " + String.valueOf(json.getJSONObject("result").get("uuid")) + " " + "5c27f9e8-2b90-433e-a0bf-b5222bbd97d0";
         autoruuid = String.valueOf(json.get("autoruuid"));
     }
-    private Integer rig , mark;
+
+    private Integer rig, mark, km_age_from, km_age_to;
+
     @Parameterized.Parameters
-    public static Collection testMark_Rig(){
+    public static Collection testMark_Rig() {
         return Arrays.asList(
                 new Object[][]{
-                        {213,15},
-                        {1,-1},
-                        {213,15}
+                        {213, 15, 10000, 20000},
+                        {1, -1, 10000, 30000},
+                        {213, 15, 10000, 40000}
                 }
         );
     }
-    public TestDDT(Integer rig, Integer mark){
+
+    public TestDDT(Integer rig, Integer mark, Integer km_age_from, Integer km_age_to) {
         this.rig = rig;
         this.mark = mark;
+        this.km_age_from = km_age_from;
+        this.km_age_to = km_age_to;
+    }
+    @Test //Тест проверяет корректность выдачи по фильтру Пробег
+    public void km_age() throws IOException {
+       //int km_age_from = 40000;
+       // int km_age_to = 42000;
+        HttpGet get = new HttpGet(url_api2+"&km_age_from="+km_age_from+"&km_age_to="+km_age_to);
+        get.setHeader("Authorization", uuid);
+        get.setHeader("X-Authorization", x_auth);
+        CloseableHttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        JSONObject jsonTets = new JSONObject(EntityUtils.toString(entity, UTF_8));
+        int lenght = jsonTets.getJSONArray("list").length();
+        for (int i = 0; i < lenght; i++) {
+            String km_ageStr = String.valueOf(jsonTets.getJSONArray("list").getJSONObject(i).get("km_age"));
+            int km_age = Integer.valueOf(km_ageStr);
+            assertTrue("Пробег выходит за параметры фильтров - " + km_age, km_age>=km_age_from & km_age<=km_age_to);
+        }
     }
     @Test
     public void test() throws IOException {
@@ -65,8 +89,8 @@ public class TestDDT {
 //        System.out.println(json);
 //        System.out.println(sid);
 //        System.out.println(uuid);
-        HttpGet get = new HttpGet("https://api2.auto.ru/1.1/search?page_num=1&page_size=20&prepend_empty_option=1&state=USED&sort=fresh_relevance_1-desc&category_id=15&photo=1&rid="+rig+"&mark_id="+mark+ "&creation_date_to=" +millis );
-       // HttpGet get = new HttpGet("https://api2.auto.ru/1.0/search/count?category_id=15&rid="+rig+"&mark_id="+mark);
+        HttpGet get = new HttpGet("https://api2.auto.ru/1.1/search?page_num=1&page_size=20&prepend_empty_option=1&state=USED&sort=fresh_relevance_1-desc&category_id=15&photo=1&rid=" + rig + "&mark_id=" + mark + "&creation_date_to=" + millis);
+        // HttpGet get = new HttpGet("https://api2.auto.ru/1.0/search/count?category_id=15&rid="+rig+"&mark_id="+mark);
         get.setHeader("Authorization", uuid);
         get.setHeader("X-Authorization", x_auth);
         get.setHeader("Content-Type", "application/json");
@@ -79,9 +103,10 @@ public class TestDDT {
 //        System.out.println(response);
 //           System.out.println(test);
         System.out.println(test.getJSONArray("list").getJSONObject(0).get("mark"));
+        System.out.println(test.getJSONArray("list").getJSONObject(0).get("model"));
         System.out.println(test.getJSONArray("list").getJSONObject(0).get("seller"));
         System.out.println(test.getJSONArray("list").getJSONObject(0).get("price"));
-       // System.out.println(get.getURI());
+        // System.out.println(get.getURI());
 //        "Accept-Encoding", "gzip, deflate, identity");
 //        get.setHeader("Accept-Language",
 
