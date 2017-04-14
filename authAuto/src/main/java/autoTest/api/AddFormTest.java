7cde33f7da8c2312e187f0904e1284224f13cc84
ihor.shchuka <ihor.shchuka@yandex.ru>
2017-04-14 14:34:24 +0300

    FormAddRefac

--- authAuto/src/main/java/autoTest/api/AddFormTest.java
+++ authAuto/src/main/java/autoTest/api/AddFormTest.java
@@ -60,38 +60,22 @@ public class AddFormTest {
 
     @Test
     public void getEditGenerations() throws IOException {
-        Response generationsList = generationsList();
-        assertTrue("catalog.folder.getEditGenerations fail, statusCode=" + generationsList.statusCode(), generationsList.statusCode() == 200);
-        String[] generationsIDList = splitToArray(generationsList.body().jsonPath().get("result.id").toString());
-        assertTrue("catalog.folder.getEditGenerations fail", generationsIDList.length > 0);
-        // System.out.println(generationsList.body().asString());
+        assertThat(splitToArray(generationsList().body().jsonPath().get("result.id").toString()).length, greaterThan(0));
     }
 
     @Test
     public void bodytype_list() throws IOException {
-        Response bodytypeList = bodytypeList();
-        assertTrue("catalog.bodytype.getList fail, statusCode=" + bodytypeList.statusCode(), bodytypeList.statusCode() == 200);
-        String[] bodytypeIDList = splitToArray(bodytypeList.body().jsonPath().get("result.id").toString());
-        assertTrue("catalog.bodytype.getList fail", bodytypeIDList.length > 0);
-        // System.out.println(bodytypeList.body().asString());
+        assertThat(splitToArray(bodytypeList().body().jsonPath().get("result.id").toString()).length, greaterThan(0));
     }
 
     @Test
     public void enginetypeGetList() throws IOException {
-        Response enginetypeList = enginetypeList();
-        assertTrue("catalog.enginetype.getList fail, statusCode=" + enginetypeList.statusCode(), enginetypeList.statusCode() == 200);
-        String[] enginetypeIDList = splitToArray(enginetypeList.body().jsonPath().get("result.id").toString());
-        assertTrue("catalog.enginetype.getList fail", enginetypeIDList.length > 0);
-        // System.out.println(enginetypeList.body().asString());
+        assertThat(splitToArray(enginetypeList().body().jsonPath().get("result.id").toString()).length, greaterThan(0));
     }
 
     @Test
     public void driveGetList() throws IOException {
-        Response driveList = driveList();
-        assertTrue("catalog.drive.getList fail, statusCode=" + driveList.statusCode(), driveList.statusCode() == 200);
-        String[] driveIDList = splitToArray(driveList.body().jsonPath().get("result.id").toString());
-        assertTrue("catalog.drive.getList fail", driveIDList.length > 0);
-        // System.out.println(driveList.body().asString());
+        assertThat(splitToArray(driveList().body().jsonPath().get("result.id").toString()).length, greaterThan(0));
     }
 
     @Test
@@ -101,10 +85,8 @@ public class AddFormTest {
 
     @Test
     public void modification_list() throws IOException {
-        Response modificationList = modificationList();
-        assertTrue("catalog.modification.getList fail, statusCode=" + modificationList.statusCode(), modificationList.statusCode() == 200);
-        String[] modificationIDList = splitToArray(modificationList.body().jsonPath().get("result.id").toString());
-        assertTrue("catalog.modification.getList fail", modificationIDList.length > 0);
+        assertThat(splitToArray(modificationList().body().jsonPath().get("result.id").toString()).length, greaterThan(0));
+
     }
 
     // Количиство ошибок валидации при размещении объявления 19
